336225dc9d274eb2c026e8ac48775c0b2c3319f3
JV658 <90655082+JV658@users.noreply.github.com>
2023-01-23 10:46:07 -0500

    Update Question 1.swift
    fixed issue with problem 1

--- Assignment1Swift/Question 1.swift
+++ Assignment1Swift/Question 1.swift
@@ -9,18 +9,38 @@ import Foundation
 class Question1Class{
 /**
  QUESTION 1:
- create the following constants or variables
+ PART 1: create the following constants or variables
  - a variable named 'myInt' of type int with a intial value of 10
  - a constant named 'myString'  of type String with no inital value
  - a variable named 'isFun' set to true
- - a variable named 'myOtherInt' set to myInt
- - change myInt to equal 14
- - a variable named 'myTwoInts' set the sum of myInt and myOtherInt
- - set myString to 'Hello World! its nice to finally try the Swift programming language. It oddly enough has no relation to Taylor Swift'
+ - a variable named 'myOtherInt' of type Int
+ - a constant named 'myTwoInts' of type Int
  - a variable named 'myArr' and set it to and empty array of integers
  */
     
     
+
+/**
+ END PART 1
+*/
+init(){
+/**
+PART 2
+ - set myOtherInt to myInt
+ - change myInt to equal 14
+ - set myTwoInts to the sum of myInt and myOtherInt
+ - set myString to 'Hello World! its nice to finally try the Swift programming language. It oddly enough has no relation to Taylor Swift'
+ */
+
+
+
+
+
+/**
+ END PART 2
+*/
+}
+
 /**
  END QUESTION 1!
  */
